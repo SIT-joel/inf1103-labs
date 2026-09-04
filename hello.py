b8b43afe77f029c191cbcd7e1db1975971afcb35
cadenzar <joel.lim04@outlook.com>
2026-09-04 16:13:05 +0800

    updated post

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-print("======================")
+print("==============================")
 print("Welcome here")
 print("My first post!")
-print("======================")
+print("==============================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
